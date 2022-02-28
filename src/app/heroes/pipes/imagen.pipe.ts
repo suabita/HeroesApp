a0bfd81e2904f4b3567f8883ed55c9432bcc8f30
karen <karen.acuna@ospinn.com>
2022-02-28 13:44:16 -0500

    CRUD

--- src/app/heroes/pipes/imagen.pipe.ts
+++ src/app/heroes/pipes/imagen.pipe.ts
@@ -3,11 +3,21 @@ import { Heroe } from '../interfaces/heroe.interface';
 
 @Pipe({
   name: 'imagen'
+  // pure: true
 })
 export class ImagenPipe implements PipeTransform {
 
-  transform(value: Heroe): string {
-    return `assets/heroes/${value.id}.jpg`;
+  transform(heroe: Heroe): string {
+
+    if( heroe.id && heroe.id.includes('-') ) {
+      console.log("pip")
+      return `assets/heroes/${heroe.id}.jpg`;
+    }else if ( heroe.alt_img ) {
+      return heroe.alt_img;
+    }else{
+      return 'assets/no-image.png'
+    }
+    
   }
 
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,12 +4,15 @@ import { CommonModule } from '@angular/common';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from "@angular/material/input";
 import { MatListModule } from '@angular/material/list';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
@@ -19,12 +22,15 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatAutocompleteModule,
     MatButtonModule,
     MatCardModule,
+    MatDialogModule,
     MatGridListModule,
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
     MatListModule,
     MatProgressSpinnerModule,
+    MatSelectModule,
+    MatSnackBarModule,
     MatSidenavModule,
     MatToolbarModule
   ],
