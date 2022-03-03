bac5135aa4e9c8becc3ea723579125f91d547485
karen <karen.acuna@ospinn.com>
2022-03-03 16:48:54 -0500

    Protección de rutas

---
+++ src/app/auth/guards/auth.guard.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
+import { Observable, tap } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements  CanLoad, CanActivate {
+
+  constructor( private authService: AuthService,
+              private router: Router ) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    // if( this.authService.auth.id ) {
+    //     return true;
+    //   }
+    // console.log("Bloqueado por el canActivate")
+    // return false;
+    return this.authService.verificaAutenticacion()
+            .pipe(
+              tap(estaAutenticado => {
+                if ( !estaAutenticado ){
+                    this.router.navigate(['./auth/login'])
+                }
+              })
+            )
+  }
+
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
+    // if( this.authService.auth.id ) {
+    //     return true;
+    //   }
+    // console.log("Bloqueado por el canLoad")
+    // return false;
+
+    return this.authService.verificaAutenticacion()
+            .pipe(
+              tap(estaAutenticado => {
+                if ( !estaAutenticado ){
+                    this.router.navigate(['./auth/login'])
+                }
+              })
+            )
+  }
+}
